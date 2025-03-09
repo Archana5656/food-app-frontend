94e860a6abc0ba149013ee26565350472f5969e1
Archana5656 <archanakvenugopal2003@gmail.com>
2025-03-09 16:53:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
